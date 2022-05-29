128243610c2ee17ec0b075725cbf6005224673b1
VASANTH <99113546+vasanthofficial@users.noreply.github.com>
2022-05-29 13:26:58 +0530

    Static-view

--- src/Components/EXPENSES/ExpenseItem.js
+++ src/Components/EXPENSES/ExpenseItem.js
@@ -1,14 +1,14 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div> {props.title}</div>
-
       <div className="expense-item__price">₹{props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
